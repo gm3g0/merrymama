00f2838f5aae8cddcf610d7663c1a5c9d82219aa
gm3g0 <kittybtsv@gmail.com>
2021-08-04 00:15:30 +0800

    完成：會員管理-會員清單、會員資料

--- resources/views/membermanage/list.blade.php
+++ resources/views/membermanage/list.blade.php
@@ -4,12 +4,53 @@
 @endsection
 
 @section('title')
-    <title>瑪利MAMA後台|</title>
+    <title>瑪利MAMA後台|會員管理</title>
 @endsection
 
 @section('main')
-<h2></h2>
-  
+<h2>會員管理</h2>
+<form action="" method="POST">
+    {{-- @method('GET') --}}
+    @csrf
+  <div class="row g-3 align-items-center float-right" style="padding: 0px;">
+    <div class="col-auto">
+        <label for="inputPassword6" class="col-form-label">電話號碼：</label>
+    </div>
+    <div class="col-auto">
+        <input type="text" value="" name="" class="form-control"id="" >
+    </div>
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary">搜尋</button>
+    </div>
+  </div><br>
+
+  <section class="table table-hover">
+    <div class="col align-self-center"> <!--時間表-->
+      <table cellpadding="0" cellspacing="0" >
+        <thead>
+          <tr class="tbl-header">
+            <th><h6><b> 名稱</th>
+            <th><h6><b> 帳號</th>
+            <th><h6><b> 電話</th>
+            <th><h6><b> 訂購次數</th>
+          </tr>
+        </thead>
+    </div>
+    <div class="tbl-content">
+        <tbody >
+                <tr>
+                    <td><a style="cursor: pointer; text-decoration:underline;color:#FEC601;font-size:16px"onclick="location.href='{{route('memberrmanage.order')}}'">名稱</a></td>
+                    <td>02</td>  
+                    <td>02</td>
+                    <td>02</td>
+                </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+
+
+</form>
   
 
 
--- resources/views/membermanage/order.blade.php
+++ resources/views/membermanage/order.blade.php
@@ -8,10 +8,80 @@
 @endsection
 
 @section('main')
-<h2></h2>
-  
-  
+<h2>會員管理</h2>
+<form id="msform" method="POST">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 text-start justify-content-center">
+        <div class="col-md-9">名稱：<input type="text" name="nstore"></div>
+        <div class="col-md-9">帳號(E-mail)：<input type="text" name="nstphone"></div>
+        <div class="col-md-9">電話：<input type="text" name="nadd"></div>
+      </div>
+    </div>
 
+  <div class="row justify-content-between">
+    <div class="col-md-2">訂購次數：</div>
+    <div class="col-md-2">累計金額：</div>
+  </div>
+</div>
+<section class="table table-hover">
+  <div class="col align-self-center"> <!--時間表-->
+    <table cellpadding="0" cellspacing="0" >
+      <thead>
+        <tr class="tbl-header">
+          <th><h6><b> 訂單編號</th>
+          <th><h6><b> 金額</th>
+          <th><h6><b> 日期</th>
+        </tr>
+      </thead>
+  </div>
+  <div class="tbl-content">
+      <tbody >
+              <tr>
+                  <td>
+                    <div class="accordion" id="accordionExample"><!--手風琴-->
+
+                      <div class="accordion-item">
+                        <h2 class="accordion-header" id="headingOne">
+                          <button class="accordion-button  collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne" >
+                            <div class="row">
+                              <div class="fs" style="font-size: 16px">訂單編號：</div>
+                            </div>
+                          </button>
+                        </h2>
+                          <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+
+                                <div class="row col-md-12 text-start align-self-center" style="margin: 0px;padding:0px">
+                                  <div class="col-md-8">
+                                    不知道不知道不知道不知道
+                                  </div>
+                                  <div class="col-md-4">
+                                    Ｘ0
+                                  </div>
+                                  <hr style="margin: 10px;width: 180px">
+                                  <div class="col-md-8">
+                                    不知道不知道
+                                  </div>
+                                  <div class="col-md-4">
+                                    Ｘ0
+                                  </div>
+                                  <hr style="margin: 10px;width: 180px">
+                                </div><br>
+
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>02</td>  
+                  <td>02</td>
+              </tr>
+      </tbody>
+    </table>
+  </div>
+</section>
+</form>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
