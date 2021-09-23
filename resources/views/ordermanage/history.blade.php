06d0e48da81c81e1b56d27dcb816fc78ecde7784
gm3g0 <kittybtsv@gmail.com>
2021-09-23 16:43:13 +0800

    我還沒用完手風琴

--- resources/views/ordermanage/history.blade.php
+++ resources/views/ordermanage/history.blade.php
@@ -20,11 +20,19 @@
 @endsection
 @section('main')
 <h2>訂單管理-歷史訂單</h2><br>
+  @php
+    $cnt = 0;
+  @endphp
 <div class="accordion" id="accordionExample"><!--手風琴-->
 @foreach($takedates as $takedate)
+  @php
+    $cnt += 1;
+    $cntt=strval($cnt);
+  @endphp
+
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOnee">
-      <button class="accordion-button  collapsed" type="button" data-toggle="collapse" data-target="#collapseOnee" aria-expanded="false" aria-controls="collapseOnee" >
+      <button class="accordion-button  collapsed" type="button" data-toggle="collapse".$cntt data-target="#collapseOnee{{$cntt}}" aria-expanded="false" aria-controls="collapseOnee" >
         <div class="row">
           <div class="fs" style="font-size: 16px">取貨日期：{{ $takedate->tekedate_time }}&nbsp;&nbsp;
           @foreach( $takedate_orders as $takedate_order )
@@ -35,20 +43,20 @@
         </div>
       </button>
     </h2>
-      <div id="collapseOnee" class="accordion-collapse collapse" aria-labelledby="headingOnee" data-bs-parent="#accordionExample">
+      <div id="collapseOnee{{$cntt}}".$cntt class="accordion-collapse collapse" aria-labelledby="headingOnee" data-bs-parent="#accordionExample">
         <div class="accordion-body">
         @foreach( $orders as $order )
           @if( $takedate->tekedate_time == $order->tekedate_time )
           <div class="accordion" id="accordionExample"><!--手風琴-->
             <div class="accordion-item">
               <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button  collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne" >
+                <button class="accordion-button  collapsed" type="button" data-toggle="collapse".$cntt data-target="#collapseOne{{$cntt}}" aria-expanded="false" aria-controls="collapseOne" >
                   <div class="row">
                     <div class="fs" style="font-size: 16px">訂單編號：{{ $order->order_id }}&nbsp;&nbsp;金額：{{ $order->total }}&nbsp;&nbsp;會員：{{ $order->email }}</div>
                   </div>
                 </button>
               </h2>
-                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="collapseOne{{$cntt}}".$cntt class="accordion-collapse collapse"aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                   <div class="accordion-body">
                     <div class="container"><br>
                     @foreach($detail_order as $detail)
@@ -76,7 +84,7 @@
         @endforeach
         </div>
       </div>
-  </div>
+  </div><br>
 @endforeach
 </div>
   
