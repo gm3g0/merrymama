95e6f8ca21faa20953e505343db25a104e867392
gm3g0 <kittybtsv@gmail.com>
2021-09-25 13:53:09 +0800

    完成：後台修改手風琴連動(除歷史訂單裡面的)、會員管理的order表格調整

--- resources/views/membermanage/order.blade.php
+++ resources/views/membermanage/order.blade.php
@@ -50,44 +50,48 @@
   </div>
   <div class="tbl-content">
       <tbody >
-            @foreach($orders as $order)
+        @php
+          $cnt = 0;
+        @endphp
+          @foreach($orders as $order)
+              @php
+                $cnt += 1;
+                $cntt=strval($cnt);
+              @endphp
               <tr>
-                  <td>
-                    <div class="accordion" id="accordionExample"><!--手風琴-->
-
-                      <div class="accordion-item">
-                        <h2 class="accordion-header" id="headingOne">
-                          <button class="accordion-button  collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne" >
-                            <div class="row">
-                              <div class="fs" style="font-size: 16px">訂單編號：{{ $order->order_id }}</div>
-                            </div>
-                          </button>
-                        </h2>
-                          <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-
-                                <div class="row col-md-12 text-start align-self-center" style="margin: 0px;padding:0px">
-                                @foreach($test as $te)
-                                  @if( $te->order_id == $order->order_id )
-                                  
-                                    <div class="col-md-8">
-                                        {{ $te->PName }}
-                                    </div>
-                                    <div class="col-md-4">
-                                        x {{ $te->num }}
-                                    </div>
-                                    
-                                    <hr style="margin: 10px;width: 180px">
-                                  @endif
-                                @endforeach
-                                </div><br>
-                            </div>
+                <td >
+                  <div class="accordion" id="accordionExample" style="width: 120%"><!--手風琴-->
+                    <div class="accordion-item">
+                      <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button  collapsed" type="button" data-toggle="collapse".$cntt data-target="#collapseOne{{$cntt}}" aria-expanded="false" aria-controls="collapseOne" >
+                          <div class="row">
+                            <div class="fs" style="font-size: 16px">訂單編號：{{ $order->order_id }}</div>
                           </div>
-                      </div>
+                        </button>
+                      </h2>
+                        <div id="collapseOne{{$cntt}}".$cntt class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                          <div class="accordion-body">
+                              <div class="row col-md-12 text-start align-self-center" style="margin: 0px;padding:0px">
+                              @foreach($test as $te)
+                                @if( $te->order_id == $order->order_id )
+                                
+                                  <div class="col-md-8">
+                                      {{ $te->PName }}
+                                  </div>
+                                  <div class="col-md-4">
+                                      x {{ $te->num }}
+                                  </div>
+                                  <hr style="margin: 10px;width: 180px">
+                                @endif
+                              @endforeach
+                              </div><br>
+                          </div>
+                        </div>
                     </div>
-                  </td>
-                  <td>{{ $order->total }}</td>  
-                  <td>{{ $order->tekedate_time }}</td>
+                  </div>
+                </td>
+                <td>{{ $order->total }}</td>  
+                <td>{{ $order->tekedate_time }}</td>
               </tr>
             @endforeach
       </tbody>
--- resources/views/ordermanage/history.blade.php
+++ resources/views/ordermanage/history.blade.php
@@ -22,12 +22,15 @@
 <h2>訂單管理-歷史訂單</h2><br>
   @php
     $cnt = 0;
+    $nn=1;
   @endphp
 <div class="accordion" id="accordionExample"><!--手風琴-->
 @foreach($takedates as $takedate)
   @php
     $cnt += 1;
+    $nn+=1;
     $cntt=strval($cnt);
+    $cnn=strval($nn);
   @endphp
 
   <div class="accordion-item">
@@ -50,13 +53,13 @@
           <div class="accordion" id="accordionExample"><!--手風琴-->
             <div class="accordion-item">
               <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button  collapsed" type="button" data-toggle="collapse".$cntt data-target="#collapseOne{{$cntt}}" aria-expanded="false" aria-controls="collapseOne" >
+                <button class="accordion-button  collapsed" type="button" data-toggle="collapse".$cntt.cnn data-target="#collapseOne{{$cntt}}{{$cnn}}" aria-expanded="false" aria-controls="collapseOne" >
                   <div class="row">
                     <div class="fs" style="font-size: 16px">訂單編號：{{ $order->order_id }}&nbsp;&nbsp;金額：{{ $order->total }}&nbsp;&nbsp;會員：{{ $order->email }}</div>
                   </div>
                 </button>
               </h2>
-                <div id="collapseOne{{$cntt}}".$cntt class="accordion-collapse collapse"aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="collapseOne{{$cntt}}{{$cnn}}".$cntt.$cnn class="accordion-collapse collapse"aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                   <div class="accordion-body">
                     <div class="container"><br>
                     @foreach($detail_order as $detail)
